a4ef8faf3ff4e9ede2ad05a56cfd48989f3ca679
justin7497 <coolkorea77@gmail.com>
2026-08-12 12:41:00 +0900

    www: sync UI 0.9.37 (app.js)

--- www/app.js
+++ www/app.js
@@ -3,7 +3,7 @@
   const $ = (id) => document.getElementById(id);
 
   /** 앱에 내장된 Web UI 번호. publishWebUi 시 서버에서 자동 증가 */
-  const WEB_UI_REVISION = 37;
+  const WEB_UI_REVISION = 38;
   const WEB_UI_REV_KEY = "naya_webui_applied_rev";
   const WEB_UI_DISMISS_KEY = "naya_webui_dismiss_rev";
   const REMOTE_WWW_FALLBACK = "https://justin7497.github.io/naya-releases/www";
